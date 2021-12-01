100d18e943e47d014a85bb451e5e1d2416f11901
ShathaAlmansour <>
2021-12-01 22:40:51 +0300

    widdleware

--- routers/middleware/authentication.js
+++ routers/middleware/authentication.js
@@ -1,20 +1,19 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const SECRET = process.env.SECRET;
-
+const secret = process.env.SECRET_KEY;
 const authentication = (req, res, next) => {
   try {
     if (!req.headers.authorization) {
       return res.status(403).json({ message: "forbidden" });
+    } else {
+      const token = req.headers.authorization.split(" ")[1];
+      const parsedToken = jwt.verify(token, secret);
+      req.token = parsedToken;
+      next();
     }
-    const token = req.headers.authorization.split(" ")[1];
-    const parsedToken = jwt.verify(token, SECRET);
-    req.token = parsedToken;
-    next();
-  } catch (err) {
-    res.status(403).json(err);
+  } catch (error) {
+    res.status(403).json(error);
   }
 };
-
 module.exports = authentication;
--- routers/middleware/authorization.js
+++ routers/middleware/authorization.js
@@ -1,17 +1,15 @@
-const roleModel = require("../../db/models/role");
-
+const roleModel = require("../db/models/role");
 const authorization = async (req, res, next) => {
   try {
-    const roleID = req.token.role;
-    const result = await roleModel.findById(roleID);
+    const roleId = req.token.role;
+    const result = await roleModel.findById(roleId);
     if (result.role === "admin") {
       next();
     } else {
-      res.status(403).json("forbidden");
+      return res.status(403).json({ message: "forbidden" });
     }
-  } catch (err) {
-    res.status(403).json(err);
+  } catch (error) {
+    return res.status(404).json(error.message);
   }
 };
-
 module.exports = authorization;
